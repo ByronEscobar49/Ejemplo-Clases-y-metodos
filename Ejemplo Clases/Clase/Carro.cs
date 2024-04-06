bf2245fd998234260b631ca1eecceb2f17ad2327
umg <umg@JUT-LAB-1H4>
2024-04-06 10:54:57 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo Clases/Clase/Carro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_Clases.Clase
+{
+    internal class Carro
+    {
+    }
+}
+public class Carro
+{
+    public string Marca { get; }
+    public int Modelo { get; set; }
+    public string Color { get; set; }
+    public string owner { get; set; }
+
+    private int Encendidio = 0;
+    private int Velocidad_actual = 0;
+    public int MAXIMAVELOCIDAD { get; set; }
+
+    public Carro(string m,int model,string col)
+    {
+        Marca = m;
+        Modelo = model;
+        Color = col;
+    }
+    public int Acelerar()
+    {
+        if  (Encendidio==0)
+            {
+            return 0;
+        }
+        else
+            Velocidad_actual += 10;
+        return Velocidad_actual;    
+    }
+}
+
+
+
---
+++ Ejemplo Clases/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Carro carrito=new Carro("Toyota",2024,"Blanco");
+Carro carro2 = new Carro("Nissan",2006,"Azul");
+
+if (carrito.Acelerar() == 0)
+{
+    Console.WriteLine("El carro esta apagado");
+}
+else
+{
+    Console.WriteLine(carrito.Acelerar());
+}
+
+Console.WriteLine(carrito.Acelerar() + "Km/h");
+Console.WriteLine(carrito.Acelerar() + "Km/h");
+Console.WriteLine(carrito.Acelerar() + "Km/h");
+
+
+
+
+
+
+
+
+
+//carrito.Marca = "Toyota";
+//carrito.Modelo = 2021;
+//carrito.Color = "azul";
+//carrito.owner = "Byron";
+//carrito.MAXIMAVELOCIDAD = 206;
+//Console.WriteLine($"El carro es un {carrito.Marca}\t" + $"modelo  {carrito.Modelo}");
+//Console.WriteLine($" segundo carro es {carro2.Marca}");
+
